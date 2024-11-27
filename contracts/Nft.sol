7f0467dc2f9fa64c7c0898381c69157ed0eed740
9ightshade <aniekemeusanga@gmail.com>
2024-11-27 19:51:36 +0100

    smart contract

--- contracts/Nft.sol
+++ contracts/Nft.sol
@@ -1,17 +1,20 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.27;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+//also removed ownable from smart contract so anyone can use our smart contract to mint nft
 
-contract MyNFT is ERC721URIStorage, Ownable {
+contract MyNFT is ERC721URIStorage, {
     uint256 private _tokenIds;
 
     constructor(address initialOwner) ERC721("MyNFT", "NFT") Ownable(initialOwner) {}
 
     function mintNFT(address recipient, string memory tokenURI)
         public
-        onlyOwner
+        /**
+        onlyOwner removing this means anyone can mint an nft with our smart contract
+        */
         returns (uint256)
     {
         _tokenIds++;
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,44 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+// import { HardhatUserConfig } from "hardhat/config";
+// import "@nomicfoundation/hardhat-toolbox";
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.27",
+// const config: HardhatUserConfig = {
+//   solidity: "0.8.27",
+// };
+
+// export default config;
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers"); // Ensure this is the correct package
+const { API_URL, PRIVATE_KEY } = process.env;
+
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.24",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.8.20",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
-
-export default config;
